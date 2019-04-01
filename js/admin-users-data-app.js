e60b1f056ef5955c98a71e418806a713a0429d88
Cyber <cybermanic@yandex.ru>
2019-04-02 00:25:09 +0500

    refactor json fields names of DataRecord, support 'modify' field

--- js/admin-users-data-app.js
+++ js/admin-users-data-app.js
@@ -20,9 +20,9 @@ class AdminUserDataApi{
         let dataRecord = {
             "id":null,
             "user_id":userId,
-            "appId":appId,
-            "dataType":dataType,
-            "jsonData":data
+            "app_id":appId,
+            "data_type":dataType,
+            "json_data":data
         }
         this.apiAdapter.postRaw('/' + userId + '/data/' + appId + '/' + dataType, data).then(resp => {
             dataRecord.id = resp.id;
@@ -62,9 +62,9 @@ Vue.component("admin-users-data-table", {
         onAddUserData(dataRecord){
             this.dataRecordList.push(dataRecord);
         },
-        onSaveUserData(dataRecord){
+        updateUserData(dataRecord){
             var pos = this.getIndexById(dataRecord.id);
-            Vue.set(this.dataRecordList, pos, dataRecord);
+            if (pos>=0) Vue.set(this.dataRecordList, pos, dataRecord);
         },
         onDeleteUserData(dataRecord){
             var pos = this.getIndexById(dataRecord.id);
@@ -74,8 +74,9 @@ Vue.component("admin-users-data-table", {
     created(){
         eventBus.$on("onFetchUserData", dataList => this.onFetchUserData(dataList));
         eventBus.$on("onAddUserData", data => this.onAddUserData(data));
-        eventBus.$on("onSaveUserData", data => this.onSaveUserData(data));
+        eventBus.$on("onSaveUserData", data => this.updateUserData(data));
         eventBus.$on("onDeleteUserData", data => this.onDeleteUserData(data));
+        eventBus.$on("onGetUserData", data => this.updateUserData(data));
         console.log('admin-users-data-table created')
     },
     template:
@@ -85,14 +86,16 @@ Vue.component("admin-users-data-table", {
             <th>User ID</th>
             <th>App ID</th>
             <th>Data type</th>
+            <th>Modified</th>
             <th>Data</th>
         </tr></thead><tbody>
         <tr v-for="dataRecord in this.dataRecordList" @click="onRowClick(dataRecord)">
             <td>{{dataRecord.id}}</td>
             <td>{{dataRecord.user_id}}</td>
-            <td>{{dataRecord.appId}}</td>
-            <td>{{dataRecord.dataType}}</td>
-            <td>{{dataRecord.jsonData}}</td>
+            <td>{{dataRecord.app_id}}</td>
+            <td>{{dataRecord.data_type}}</td>
+            <td>{{dataRecord.modified}}</td>
+            <td>{{dataRecord.json_data}}</td>
             <td><button class="btn btn-default btn-xs" @click.stop="onDeleteClick(dataRecord)">Удалить</button></td>
         </tr></tbody></table>`
 });
@@ -112,9 +115,10 @@ Vue.component("user-data-details", {
             this.dataRecord = {
                 "id":"",
                 "user_id":"",
-                "appId":"",
-                "dataType":"",
-                "jsonData":"",
+                "app_id":"",
+                "data_type":"",
+                "modofied":"",
+                "json_data":"",
             };
             this.styles = {
             }
@@ -143,11 +147,16 @@ Vue.component("user-data-details", {
                 adminUserDataApi.save(this.dataRecord);
                 this.reset();
             }else{
-                var appId = (this.dataRecord.appId>0)? this.dataRecord.appId: 0;
-                var dataType = (this.dataRecord.dataType>0)? this.dataRecord.dataType: 0;
-                adminUserDataApi.add(this.dataRecord.user_id, appId, dataType, this.dataRecord.jsonData);
+                var appId = (this.dataRecord.app_id>0)? this.dataRecord.app_id: 0;
+                var dataType = (this.dataRecord.data_type>0)? this.dataRecord.data_type: 0;
+                adminUserDataApi.add(this.dataRecord.user_id, app_id, data_type, this.dataRecord.json_data);
                 this.reset();
             }
+        }
+    },
+    computed:{
+        isNew:function(){
+            return !(this.dataRecord.id>0);
         }
     },
     created(){
@@ -166,25 +175,29 @@ Vue.component("user-data-details", {
                 </div>
                 <div class="form-group">
                     <label for="user_id" class="col-md-3 control-label">User ID</label>
-                    <div class="col-md-9"><input name="user_id" class="form-control" v-model="dataRecord.user_id"></input></div>
+                    <div class="col-md-9"><input name="user_id" class="form-control" v-model="dataRecord.user_id" :disabled="!isNew"></input></div>
                 </div>
                 <div class="form-group">
                     <label for="app_id" class="col-md-3 control-label">App ID</label>
-                    <div class="col-md-9"><input name="app_id" class="form-control" v-model="dataRecord.appId"></input></div>
+                    <div class="col-md-9"><input name="app_id" class="form-control" v-model="dataRecord.app_id" :disabled="!isNew"></input></div>
                 </div>
                 <div class="form-group">
-                    <label for="data_type" class="col-md-3 control-label">dataType</label>
-                    <div class="col-md-9"><input name="data_type" class="form-control" v-model="dataRecord.dataType"></input></div>
+                    <label for="data_type" class="col-md-3 control-label">Data type</label>
+                    <div class="col-md-9"><input name="data_type" class="form-control" v-model="dataRecord.data_type" :disabled="!isNew"></input></div>
+                </div>
+                <div class="form-group">
+                    <label for="timestamp" class="col-md-3 control-label">Modified</label>
+                    <div class="col-md-9"><input name="timestamp" class="form-control" v-model="dataRecord.modified" disabled></input></div>
                 </div>
             </div>
             <div class="col-md-8">
                 <div class="form-group">
-                    <div class="col-md-12"><textarea name="json_data" class="form-control" rows="8" v-model="dataRecord.jsonData"></textarea></div>
+                    <div class="col-md-12"><textarea name="json_data" class="form-control" rows="10" v-model="dataRecord.json_data"></textarea></div>
                 </div>
             </div>
         </div>
         <div class="text-center">
-            <button class="btn btn-default" @click="reset">Очистить</button>
+            <button class="btn btn-default" @click="reset">Сброс</button>
             <button class="btn btn-success" @click="commit">Сохранить</button>
         </div>
     </div>
@@ -196,8 +209,8 @@ Vue.component('user-data-query',{
         return{
             "usersList":[],
             "user_id":"",
-            "appId":"",
-            "dataType":"",
+            "app_id":"",
+            "data_type":"",
             "selectedUser":"",
         }
     },
@@ -210,16 +223,16 @@ Vue.component('user-data-query',{
         reset(){
             this.userList=[];
             this.user_id="";
-            this.appId="";
-            this.dataType="";
+            this.app_id="";
+            this.data_type="";
             this.selectedUserHolder="";
         },
         startQuery(){
-            console.log("startQuery() ", this.user_id, this.appId, this.dataType);
-            if(this.user_id!='' && this.appId!='' && this.dataType!=''){
-                adminUserDataApi.fetchAllByDataType(this.user_id, this.appId, this.dataType);
-            }else if(this.appId!='' && this.user_id!=''){
-                adminUserDataApi.fetchAllByAppId(this.user_id, this.appId);
+            console.log("startQuery() ", this.user_id, this.app_id, this.data_type);
+            if(this.user_id!='' && this.app_id!='' && this.data_type!=''){
+                adminUserDataApi.fetchAllByDataType(this.user_id, this.app_id, this.data_type);
+            }else if(this.app_id!='' && this.user_id!=''){
+                adminUserDataApi.fetchAllByAppId(this.user_id, this.app_id);
             }else if(this.user_id!=''){
                 adminUserDataApi.fetchAllByUserId(this.user_id)
             }else{
@@ -229,7 +242,7 @@ Vue.component('user-data-query',{
         validateInput(){
             if (this.selectedUser.id!=this.user_id) this.selectedUser = "";
             if (''==this.user_id) return false;
-            if (''==this.appId && ''!=this.dataType) return false;
+            if (''==this.app_id && ''!=this.data_type) return false;
             return true;
         },
         onFetchUsers(usersList){
@@ -250,8 +263,8 @@ Vue.component('user-data-query',{
             <div class="form-group"><button class="btn btn-default" @click="reset">Сброс</button></div>
             <div class="form-group"><select class="form-control" v-model="selectedUser" @change="onSelectUser"><option v-for="u in usersList" :value="u">{{u.login}}</option></select></div>
             <div class="form-group"><input name="user_id" class="form-control" placeholder="User ID" v-model="user_id" @changed='validateInput' @keyup.enter='startQuery'></input></div>
-            <div class="form-group"><input name="app_id" class="form-control" placeholder="App ID" v-model="appId" @changed='validateInput'></input></div>
-            <div class="form-group"><input name="data_type" class="form-control" placeholder="Data Type" v-model="dataType" @changed='validateInput'></input></div>
+            <div class="form-group"><input name="app_id" class="form-control" placeholder="App ID" v-model="app_id" @changed='validateInput'></input></div>
+            <div class="form-group"><input name="data_type" class="form-control" placeholder="Data Type" v-model="data_type" @changed='validateInput'></input></div>
             <div class="form-group"><button class="btn btn-primary" @click="startQuery" :disabled="!readyForQuery">Поиск</button></div>
         </div>
     </div>
